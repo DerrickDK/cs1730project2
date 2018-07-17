9b1e6602b27f7e435dde340619994c370ab78c74
Derrick Kyei <derrickdkyei@yahoo.com>
2018-07-16 21:47:07 -0400

    Updated wc.pp

--- wc.cpp
+++ wc.cpp
@@ -1,32 +1,103 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cctype>
 #define BUFFSIZE 4096
 
 using namespace std; 
 int main(int argc, char *argv[]){
-  int i;
-  char buffer [BUFFSIZE];
+  char letter = argv[1][1];
+  char **argument = argv;
+  int total = 0;
+  int index = 2;
 
-  while ((n = read(STDIN_FILENO, buffer, BUFFSIZE)) > 0){
-    write(STDOUT_FILENO, buffer, n);
+  if(argument && (argc < 3)){
+    int n;
+    char buff[BUFFSIZE];
+    while((n = read(STDIN_FILENO, buff, BUFFSIZE)) > 0 ) {
+      if(write(STDOUT_FILENO, buff, n) != n) {
+	cerr << "error writing" << endl;
+	exit(1);
+      }
+    }
+  }//if
+
+  while(index < argc ){
+    int counter = 0;
+    switch(letter){
+    case 'c':
+      {
+	int fd, n;
+	char buff[BUFFSIZE];
+	fd = open(argument[index], O_RDONLY);
+	if(fd == -1){
+	  perror("No such file exist");
+	  exit(1);
+	}
+	while ((n = read(fd, buff, BUFFSIZE)) > 0){
+	  counter+=n;
+	}
+	printf("Number of characters in %s is: %d\n", argument[index], counter);
+	break;
+      }
+    case 'l':
+      {
+	int fd, n; 
+	char buff[BUFFSIZE];
+	fd = open(argument[index], O_RDONLY);
+	if(fd == -1){
+	  perror("No such file exist");
+	  exit(2);
+
+	}
+	while((n = read(fd, buff, BUFFSIZE)) > 0){
+	  cout << n << endl;
+	  for(int i = 0; i < n; i++){
+	    if(buff[i] == '\n'){
+	      counter++;
+	    }
+	  }
+	}
+	printf("Number of lines in %s is %d\n", argument[index], counter);
+	break;
+      }
+  case 'w'://a word is a sequence of NON- white spaces
+    {      
+      int fd, n; 
+      char buff[BUFFSIZE];
+      bool ifWord = false; 
+      fd = open(argument[index], O_RDONLY);
+      if(fd == -1){
+	perror("No such file exit");
+	exit(3);
+      }
+      while((n = read(fd, buff, BUFFSIZE)) > 0){
+	for(int i =0; i < n; i++){
+	  if(isspace(buff[i])){
+	     ifWord = false; 
+	  }
+	  else if(!(isspace(buff[i])) && !(ifWord)){
+	    ifWord = true; 
+	    counter++;
+	  }
+	  else if(!(isspace(buff[i])) && ifWord){ 
+	    continue;
+	  }
+	}//for
+      }//while
+      printf("Number of words in %s is %d\n",argument[index], counter);
+      break;
+    }
+  default:
+    {
+      perror("No such command exist");
+    }
+  }//switch
+  total+=counter;
+  if(argc > 3){
+    printf("Total is %d\n", total);
   }
-  return n;
-  /*  std:tstring letter = argv[1]; 
-  switch(letter){
-  case "-w":
-
-    break;
-  case "-l":
-
-    break;
-  case "-c":
-
-    break;
-  }
-  for(i=0; i < argc; i++){
-    cout << argv[i] <<endl;
-
-  }
-  int file = open(argv[2]);*/
-
-
-}
+  index++;
+  }//outer while
+}//main
