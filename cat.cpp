6f2eaee252483d11658303e78a158feafd671ae6
Derrick Kyei <derrickdkyei@yahoo.com>
2018-07-16 21:45:58 -0400

    Updated cat

--- cat.cpp
+++ cat.cpp
@@ -7,35 +7,36 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <cstring>
-#include <string>
-#define BUFFSIZE 4096
+#define BUFFSIZE 4
 using namespace std; 
 
 int main(int argc, char *argv[]){
   int i; 
-  
   char buff[BUFFSIZE];
-  for(i = 1; i < argc; i++){    
-    // int fd = open(argv[i], O_RDONLY);
-    if(!(strcmp(argv[i], "-"))){
-      int m;
-      while((m = read(STDIN_FILENO, buff, BUFFSIZE)) > 0 ) {
-	if(write(STDOUT_FILENO, buff, m) != m) {
-	}
-      }
-    }
-  }
-      for(i = 1; i < argc; i++) {
-      int fd = open(argv[i], O_RDONLY);
+  char **arguments;
+  arguments =argv;
+
+  for(i = 1; i < argc; i++){
+    if(strcmp(arguments[i],"-") == 0){
+      int n;
+      char buff[BUFFSIZE];
+      while((n = read(STDIN_FILENO, buff, BUFFSIZE)) > 0 ) {
+	if(write(STDOUT_FILENO, buff, n) != n) {
+	  cerr << "error writing" << endl;
+	  exit(1);
+	}//inner if
+      }//while
+    }//outer if
+    else{
+    int fd = open(arguments[i], O_RDONLY);
     if(fd != -1){
       int n;
       while((n = read(fd, buff, BUFFSIZE)) > 0){
-	write(STDOUT_FILENO, buff, n);	
-       }
-     }
-   }
-
-  
-
-  
-} 
+	write(STDOUT_FILENO, buff, n);
+      }
+    }else{
+      perror("Error");
+    }
+    }//else
+  }
+}//main
